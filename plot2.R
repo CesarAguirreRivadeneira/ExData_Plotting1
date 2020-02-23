b9a0650ed8134b2050cdc7d929c9b2381bca0064
Cesar Aguirre <cesar.aguirre.rivadeneira@gmail.com>
2020-02-23 11:26:23 +0100

    Apagon de ordenador

--- plot2.R
+++ plot2.R
@@ -1,7 +1,9 @@
 file <- "household_power_consumption.txt"
 HousePWCons <- read.table(file = file,header = TRUE, sep=";",na.strings = "?",stringsAsFactors = FALSE,dec = ".",colClasses=c(rep("character",2),rep("numeric",7)))
 HousePWCons<-subset(HousePWCons,Date == "1/2/2007" | Date == "2/2/2007")
+
+columna <- strptime(paste(HousePWCons$Date,HousePWCons$Time),format = "%e/%m/%Y %H:%M:%S")
+
 png(file = "plot1.png", width = 480, height = 480)
-hist(HousePWCons$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
-#dev.print(png, file = "plot1.png", width = 480, height = 480)
-dev.off()
+
+
